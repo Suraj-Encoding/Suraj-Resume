d6eda10b27ce9e79122037c83fc3e9249b1c15d9
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-06-04 17:31:04 +0530

    chore: Add "use client" import to BG, Footer, Tooltip, and createTooltip components

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import { Data } from '@/Interface/constant/data';
 
--- Components/Tooltip/Tooltip.tsx
+++ Components/Tooltip/Tooltip.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { ToolTip } from '../../Interface/types/tooltip';
 import ReactTooltip from 'react-tooltip';
