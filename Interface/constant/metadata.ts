67c45b1979421fb9d879a1274aba9d9703858a91
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-06-04 15:21:18 +0530

    chore: Update metadata descriptions for Suraj Dalvi pages

--- Interface/constant/metadata.ts
+++ Interface/constant/metadata.ts
@@ -3,12 +3,12 @@ import { MetaDataProps } from '../types/metadata';
 export const MetaData: MetaDataProps[] = [
   {
     title: "Suraj Dalvi - Resume",
-    description: "🕸️ Suraj Dalvi - Resume Page 🕸️",
+    description: "💲Suraj Dalvi💲 ~ 🕸️ Resume Page 🕸️",
     icons: "/Logo.png"
   },
   {
     title: "Suraj Dalvi - Contact",
-    description: "🕸️ Suraj Dalvi - Resume Page 🕸️",
+    description: "💲Suraj Dalvi💲 ~ 🕸️ Resume Page 🕸️",
     icons: "/Logo.png"
   }
 ];
