00d1fd783099c9d0d8b3aff21f39da7dbcb169f6
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-06-13 12:13:09 +0530

    chore: Comment out output setting for Clerk middleware

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // # comment out for clerk middleware 
+  // output: "export", 
   trailingSlash: true,
   images: {
     unoptimized: true
