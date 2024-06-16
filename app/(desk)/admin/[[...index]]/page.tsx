aaf73fbd95cb5fd9c5368ff80743704726e98c5f
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-06-16 16:59:34 +0530

    chore: Update import statements in StudioPage component

--- app/(desk)/admin/[[...index]]/page.tsx
+++ app/(desk)/admin/[[...index]]/page.tsx
@@ -1,12 +1,12 @@
 // # Sanity Studio #
 
+"use client";
+
 import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../sanity.config';
+import config from '@/sanity.config';
 
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
-
-export default function StudioPage() {
+const Admin = () => {
   return <NextStudio config={config} />
 }
+
+export default Admin;
