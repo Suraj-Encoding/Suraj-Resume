f5de8dbd16ab2cdf76dce57b3ccd108dd51e46ab
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-08-19 11:56:12 +0530

    updated sign-up component and all clerk settings

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,78 +1,11 @@
-// app/sign-up/[[...sign-up]]/page.tsx
-"use client";
-import Link from "next/link";
-import { FormEvent, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useClerk, useSignUp } from "@clerk/nextjs";
-import SignupForm from './form';
-import VerifyForm from './verify';
+import { SignUp } from "@clerk/nextjs";
 
-const Signup = () => {
-  const { isLoaded, signUp, setActive } = useSignUp();
-  const [clerkError, setClerkError] = useState("");
-  const router = useRouter();
-  const [verifying, setVerifying] = useState(false);
-  const [code, setCode] = useState("");
-
-  const { signOut } = useClerk();
-
-  const signUpWithEmail = async ({
-    emailAddress,
-    password,
-  }: {
-    emailAddress: string;
-    password: string;
-  }) => {
-    if (!isLoaded) {
-      return;
-    }
-
-    try {
-      await signUp.create({
-        emailAddress,
-        password,
-      });
-      // send the email.
-      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
-
-      // change the UI to our pending section.
-      setVerifying(true);
-    } catch (err: any) {
-      setClerkError(err.errors[0].message);
-    }
-  };
-
-  const handleVerify = async (e: FormEvent) => {
-    e.preventDefault();
-    if (!isLoaded) return;
-
-    try {
-      const completeSignUp = await signUp.attemptEmailAddressVerification({
-        code,
-      });
-      if (completeSignUp.status !== "complete") {
-        console.log(JSON.stringify(completeSignUp, null, 2));
-      }
-
-      if (completeSignUp.status === "complete") {
-        await setActive({ session: completeSignUp.createdSessionId });
-        router.push("/sign-in");
-        signOut();
-      }
-    } catch (err) {
-      console.log("Error:", JSON.stringify(err, null, 2));
-    }
-  };
-
+const SignUpPage = () => {
   return (
     <>
-      {!verifying ?
-        (<SignupForm signUpWithEmail={signUpWithEmail} clerkError={clerkError} />) :
-        (<VerifyForm handleVerify={handleVerify} code={code} setCode={setCode} />)
-      }
+      <SignUp />
     </>
-  )
-
+  );
 };
 
-export default Signup;
+export default SignUpPage;
